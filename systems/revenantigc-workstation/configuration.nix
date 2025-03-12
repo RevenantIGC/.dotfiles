8918f63dad99bd0b67a59d1a6847e7c3056b4491
Igor Alves da Costa <igoralvescosta666@gmail.com>
2025-03-12 17:53:34 -0300

    try #3 fix display2

--- systems/revenantigc-workstation/configuration.nix
+++ systems/revenantigc-workstation/configuration.nix
@@ -52,7 +52,7 @@
     qpwgraph -a &
     xrandr --output DP-1 --primary --mode 1920x1080 --output HDMI-1 --right-of DP-1 --mode 1920x1080
   '';
-  services.libinput.mouse.accelSpeed = "-0.5";
+  services.libinput.mouse.accelSpeed = "-1";
   services.picom = {
     enable = true;
     vSync =true;
